ab1621e57e78f595d6b7c329fecc53d4327a5144
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-09-19 19:12:36 -0500

    app.js work

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -5,10 +5,13 @@ var app = express();
 // "/" -> "Hi there!"
 app.get("/", function(req, res) {
     res.send("Hi there!");
-
 });
 
 // "bye" => "Goodbye!"
+app.get("/bye", function(req, res){
+    res.send("Goodbye!!");
+});
+
 // "/dog" => "Meow!"
 
 app.listen(process.env.PORT, process.env.IP, function() { // type ->  "PORT=3000 node app.js" in terminal to start 
